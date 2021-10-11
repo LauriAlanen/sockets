e2324013e48cf74b74bbdf97bb89e8fa71c0d51b
Lauri Alanen <80245457+LauriAlanen@users.noreply.github.com>
2021-10-11 16:07:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,69 +1,75 @@
 import socket
 import threading
+import sys
 import webbrowser
+
+
+class colors:
+    RED   = "\033[1;31m"  
+    BLUE  = "\033[1;34m"
+    CYAN  = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD    = "\033[;1m"
+    REVERSE = "\033[;7m"
+
+sys.stdout.write(colors.RED)
 
 HEADER = 8
 PORT = 5050
-SERVER = "your-ip"
-CLIENTIP = "clients-ip"
+SERVER = "192.168.190.189"
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!Disconnect"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind(ADDR)
 
-def handle_connection(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    def handle_connection(conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
 
-    connected = True
-    while(connected):
-        msg_lenght = conn.recv(HEADER).decode(FORMAT)
-        if msg_lenght:
-            msg_lenght = int(msg_lenght)
-            msg = conn.recv(msg_lenght).decode(FORMAT)
-            if msg == DISCONNECT_MSG:
-                connected = False
-            if msg == "Youtube":
-                webbrowser.open("https://www.youtube.com/")
-            if msg == "Reddit":
-                webbrowser.open("https://www.reddit.com/")
-            confirm(conn, msg)
-            print(f"[{addr}] {msg}")
-
-
-    conn.close()
-
-
-def start():
-    server.listen() 
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_connection, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        connected = True
+        while(connected):
+            msg_lenght = conn.recv(HEADER).decode(FORMAT)
+            match msg_lenght:
+                case _:
+                    msg_lenght = int(msg_lenght)
+                    msg = conn.recv(msg_lenght).decode(FORMAT)
+                    match msg:
+                        case "Youtube":
+                            webbrowser.open("https://www.youtube.com/")
+                        case "Reddit":
+                            webbrowser.open("https://www.reddit.com/")
+                        case "!Disconnect":
+                            connected = False
+                    confirm(conn, msg)
+                    print(f"Message received[{addr}] Message: {msg} ")
+        conn.close()
 
 
-def confirm(conn, msg):
-    def send_msg(callback):
+    def start():
+        server.listen() 
+        print(f"[LISTENING] Server is listening on {SERVER}")
+        while True:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_connection, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+    def confirm(conn, msg):
+        match msg:
+            case "!Disconnect":
+                callback = "[SERVER] Disconnection confirmed!".encode(FORMAT)
+            case _:
+                callback = "[SERVER] Message received!".encode(FORMAT)
+        
         callback_len = len(callback)
         send_lenght = str(callback_len).encode(FORMAT)
-        #send_lenght += b" " * (HEADER - len(send_lenght))
         send_lenght += send_lenght.ljust(HEADER - len(send_lenght))
         conn.send(send_lenght)
         conn.send(callback)
-    if(msg == DISCONNECT_MSG):
-        callback = "[SERVER] Disconnection confirmed!".encode(FORMAT)
-        send_msg(callback)
-    else:
-        callback = "[SERVER] Message received!".encode(FORMAT)
-        send_msg(callback)
 
-
-
-
-
-print("[STARTING] server is starting...")
-
-start()
+    if __name__ == '__main__':
+        print("[STARTING] Server is starting...")
+        start()
